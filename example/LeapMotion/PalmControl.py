4c7be646a865a3def760891bfb75d6e64023baea
Poopeye <bxw229@case.edu>
2015-12-12 18:29:33 -0500

    palm control with leapmotion

--- example/LeapMotion/PalmControl.py
+++ example/LeapMotion/PalmControl.py
@@ -70,7 +70,12 @@ def main():
     listener = SampleListener()
     controller = Leap.Controller()
 
-    MotorSpeed(Motors, 150)
+    MotorSpeed(Motors, 120)
+    dxl_io.set_goal_position({1: 0})
+    dxl_io.set_goal_position({3: 90})
+    dxl_io.set_goal_position({5: 0})
+    dxl_io.set_goal_position({7: 0})
+    time.sleep(3)
     HandsNum = 0
     while HandsNum != 2:
         # Have the sample listener receive events from the controller
@@ -87,24 +92,65 @@ def main():
         #             finger.length,
         #             finger.width)
 
-        for gesture in frame.gestures():
-            if gesture.type == Leap.Gesture.TYPE_CIRCLE:
-                circle = CircleGesture(gesture)
+        for hand in frame.hands:
+            xPalm = hand.palm_position[0]
+            yPalm = hand.palm_position[1]
+            zPalm = hand.palm_position[2]
 
-                # Determine clock direction using the angle between the pointable and the circle normal
-                if circle.pointable.direction.angle_to(circle.normal) <= Leap.PI/2:
-                    clockwiseness = "clockwise"
-                    dxl_io.set_goal_position({1: 0})
-                    dxl_io.set_goal_position({3: 90})
-                    dxl_io.set_goal_position({5: 90})
-                    dxl_io.set_goal_position({7: 90})
+        if xPalm <= -90:
+            J1pos = -90
+            dxl_io.set_goal_position({1: J1pos})
+        elif xPalm >= 90:
+            J1pos = 90
+            dxl_io.set_goal_position({1: J1pos})
+        else:
+            J1pos = (-xPalm) // 1
+            dxl_io.set_goal_position({1: J1pos})
+        print(J1pos)        
 
-                else:
-                    clockwiseness = "counterclockwise"
-                    dxl_io.set_goal_position({1: 0})
-                    dxl_io.set_goal_position({3: 0})
-                    dxl_io.set_goal_position({5: 0})
-                    dxl_io.set_goal_position({7: 0})
+        if yPalm <= 45:
+            J2pos = 45
+            dxl_io.set_goal_position({3: J2pos})
+        elif yPalm >= 135:
+            J2pos = 0
+            dxl_io.set_goal_position({3: J2pos})
+        else:
+            J2pos = (135 - yPalm) // 1
+            dxl_io.set_goal_position({3: J2pos})
+        print(J2pos)
+
+        if zPalm <= -90:
+            J3pos = -90
+            dxl_io.set_goal_position({5: J3pos})
+        elif zPalm >= 90:
+            J3pos = 90
+            dxl_io.set_goal_position({5: J3pos})
+        else:
+            J3pos = zPalm // 1
+            dxl_io.set_goal_position({5: J3pos})
+        print(J3pos)
+
+
+
+
+        # for gesture in frame.gestures():
+        #     if gesture.type == Leap.Gesture.TYPE_CIRCLE:
+        #         circle = CircleGesture(gesture)
+
+        #         # Determine clock direction using the angle between the pointable and the circle normal
+        #         if circle.pointable.direction.angle_to(circle.normal) <= Leap.PI/2:
+        #             clockwiseness = "clockwise"
+        #             dxl_io.set_goal_position({1: 0})
+        #             dxl_io.set_goal_position({3: 90})
+        #             dxl_io.set_goal_position({5: 90})
+        #             dxl_io.set_goal_position({7: 90})
+
+        #         else:
+        #             clockwiseness = "counterclockwise"
+        #             dxl_io.set_goal_position({1: 0})
+        #             dxl_io.set_goal_position({3: 0})
+        #             dxl_io.set_goal_position({5: 0})
+        #             dxl_io.set_goal_position({7: 0})
 
 
 
