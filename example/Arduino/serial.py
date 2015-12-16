4294052161d19cfc4c7fea143d444506555870b9
Poopeye <bxw229@case.edu>
2015-12-16 07:05:19 -0500

    include arduino

--- example/Arduino/serial.py
+++ example/Arduino/serial.py
@@ -4,5 +4,9 @@ __author__ = 'Bowen Weng'
 
 import serial
 arduino = serial.Serial('/dev/tty.usbxxx, 9600')
+ardata = [0]
 while True:
-	print arduino.readline()=
+	print arduino.readline()
+	ardata.append(arduino.readline())
+ardata.pop(0)
+print(ardata)
